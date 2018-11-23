a4da6a73910322da0a005478322b8513b6f406b5
ming <jianmingforpc@gmail.com>
2018-11-23 14:48:56 +0800

    删掉无用注释

--- main.go
+++ main.go
@@ -3,22 +3,9 @@ package main
 import (
 	"hadoop-fs/fs"
 	"hadoop-fs/fs/config"
-	// _ "net/http/pprof"
 )
 
 func main() {
-
-	// go func() {
-	// 	log.Println(http.ListenAndServe("localhost:6060", nil))
-	// }()
-	// cg := config.Config{}
-	// cg.Attrtimeout = 1
-	// cg.Hadoop.Host = "192.168.50.254"
-	// cg.Hadoop.Port = 50070
-	// cg.Hadoop.Username = "ming"
-	// cg.Mountpoint = "/home/ming/golang/project/src/transfer-fs/test"
-	// cg.Debug = true
-	// cg.NotExistCacheTimeout = 200
 
 	cg := config.ParseFromCmd()
 	fs.Service(cg)
